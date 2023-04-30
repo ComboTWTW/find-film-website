7104664dd3454f04c175f8bf26b014b1ce959664
ComboTWTW <gfakemail18@gmail.com>
2023-05-01 02:09:49 +0500

    Responsive slider with arrows and drag func-ty

--- src/components/Home/HeroSwiper.tsx
+++ src/components/Home/HeroSwiper.tsx
@@ -2,13 +2,13 @@ import { useEffect, useState } from 'react';
 import { getPopular } from '../../api/getPopular' 
 import { useQuery } from 'react-query';
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Pagination, Navigation } from "swiper";
+import { Pagination, Navigation, Autoplay } from "swiper";
+import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
 
 // Import Swiper styles
 import "swiper/css";
 import "swiper/css/pagination";
-import "swiper/css/navigation";
-import { getFilm } from '../../api/getFilm';
+import "../../index.css";
 import SubSlide from './SubSlide';
 
 
@@ -41,28 +41,55 @@ const HeroSwiper = () => {
       refetchPopular();
     }, [])
     
+    
 
   return (
     <div className='flex w-full justify-center '>
+        <div className="relative w-full flex justify-center">
         <Swiper
           slidesPerView={2}
           spaceBetween={20}
           loop={true}
-          navigation={true}
-          modules={[Navigation]}
-          centeredSlides={true}
-          
+          modules={[Navigation, Autoplay]}
+          autoplay={{
+            delay: 2500,
+            disableOnInteraction: false,
+          }}
 
-          breakpoints={{}}
-          className="min-w-[115vw]"
+          navigation={{
+            enabled: true,
+          }}
+          centeredSlides={true}
+    
+          breakpoints={{
+            320: {
+              slidesPerView: 1,
+              pagination: false,
+              autoplay: {
+
+                delay: 2500,
+                disableOnInteraction: false,
+              },
+            },
+            768: {
+              slidesPerView: 2,
+              navigation: {
+                nextEl: ".image-swiper-button-next",
+                prevEl: ".image-swiper-button-prev",
+                enabled: true,
+              }
+          
+            },
+          }}
+          className="md:min-w-[115vw]" 
         >
             {isSuccessPopular && dataPopular?.results.slice(0, 10).map((film:popularDataT) => (
-              <SwiperSlide className={`overflow-hidden flex justify-center cursor-pointer`} key={`https://image.tmdb.org/t/p/original${film.backdrop_path}`}>
+              <SwiperSlide className={` overflow-hidden flex justify-center cursor-pointer relative`} key={`https://image.tmdb.org/t/p/original${film.backdrop_path}`}>
                {({isPrev, isNext, isActive}) => (
                <div className={`${isNext && 'opacity-50'} ${isPrev && 'opacity-50'} relative rounded-[10px] flex justify-center items-center overflow-hidden w-full `}>
                   <img loading='lazy' className='w-full h-auto' src={`https://image.tmdb.org/t/p/original${film.backdrop_path}`} alt="" />
                   <div className="absolute left-0 bottom-0 top-0 bg-black bg-opacity-90 p-4 pr-10 flex flex-col justify-between items-start max-w-[45%] min-w-[45%]">
-                    <h1 className=' shrink-1 poppins text-white md:text-lg lg:text-4xl  font-bold'>{film.title}</h1>
+                    <h1 className=' shrink-1 poppins text-white md:text-lg lg:text-4xl lg:leading-normal md:leading-snug font-bold'>{film.title}</h1>
                     <SubSlide posterPath={film.poster_path} id={film.id}/>
                   </div>
                 </div>
@@ -70,6 +97,9 @@ const HeroSwiper = () => {
               </SwiperSlide>
             ))}
         </Swiper>
+        <div className="image-swiper-button-prev hidden md:absolute z-[51] md:flex swiper-button left-5 opacity-50 hover:opacity-100 hover:duration-300 cursor-pointer top-[40%]"><IoIosArrowBack /></div>
+        <div className="image-swiper-button-next hidden md:absolute z-[51] md:flex swiper-button right-5 opacity-50 hover:opacity-100 hover:duration-300 cursor-pointer top-[40%]"><IoIosArrowForward /></div>
+        </div>
     </div>
   )
 }
--- src/components/Home/SubSlide.tsx
+++ src/components/Home/SubSlide.tsx
@@ -36,12 +36,12 @@ const SubSlide = ({ posterPath, id }:Props) => {
   return (
     <div className="flex w-full mt-3 gap-4">
 
-        <div className="flex justify-center items-center min-w-[40%] max-w-[45%] ">
-            <img loading="lazy" key={posterPath} src={`https://image.tmdb.org/t/p/original${posterPath}`} alt="" className='w-full h-auto rounded-[10px]'/>
+        <div className="sm:hidden md:flex justify-center items-center min-w-[40%] max-w-[45%] ">
+            <img loading="lazy" key={posterPath} src={`https://image.tmdb.org/t/p/w500${posterPath}`} alt="" className='w-full h-auto rounded-[10px]'/>
         </div>
 
         {isSuccessFilm && <div className="flex flex-col ">
-            <p className="poppins text-white text-xs lg:text-base ">{dataFilm.release_date.slice(0, 4)} <span className={`${dataFilm.runtime === 0 && 'hidden'}`}> • {Math.floor(dataFilm.runtime /60)} hr {dataFilm.runtime % 60}</span> min</p>
+            <p className="poppins text-white text-xs lg:text-base ">{dataFilm.release_date.slice(0, 4)} <span className={`${dataFilm.runtime === 0 && 'hidden'}`}> • {Math.floor(dataFilm.runtime /60)} hr {dataFilm.runtime % 60} min</span></p>
             <p className="poppins text-white text-xs lg:text-base mt-[5px] ">{dataFilm.genres.slice(0, 1).map((genre) => {
                 return `${genre.name}`
             })
