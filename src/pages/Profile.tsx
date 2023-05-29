179da941924f7ff42c5874ea29ee17d626f317de
ComboTWTW <gfakemail18@gmail.com>
2023-05-29 13:55:15 +0500

    Log out button in Profile

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -5,6 +5,7 @@ import { doc, getDoc } from "firebase/firestore";
 import { auth, db } from "../config/firebase";
 import { onAuthStateChanged } from "firebase/auth";
 import CurrentList from "../components/Profile/CurrentList";
+import { signOut } from "firebase/auth";
 
 const Profile = () => {
     const [lists, setLists] = useState<object | undefined>();
@@ -21,6 +22,15 @@ const Profile = () => {
         });
     }, [currentList]);
 
+    const signOutCall = async () => {
+        try {
+            await signOut(auth);
+            window.location.reload();
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
     return (
         <div className="w-full flex justify-center">
             <div className="max-w-[1300px] px-4 w-full flex flex-col">
@@ -35,9 +45,16 @@ const Profile = () => {
                                 lists={lists}
                                 setCurrentLists={setCurrentList}
                             />
+
+                            <button
+                                onClick={() => signOutCall()}
+                                className="hidden md:block poppins text-red-700 text-lg mt-16 font-medium border-red-700 border-solid border-[2px] rounded-[5px] px-4 py-2"
+                            >
+                                Log Out
+                            </button>
                         </div>
 
-                        <div className="w-full mt-5 md:mt-0">
+                        <div className="w-full mt-5 md:mt-0 flex flex-col">
                             <CurrentList
                                 lists={lists}
                                 currentList={currentList}
