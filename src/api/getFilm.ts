ef2ad05a28e0f12b2f5812a12ad9a3c650de977e
ComboTWTW <gfakemail18@gmail.com>
2023-06-13 06:19:30 +0500

    GetFilm API function type update

--- src/api/getFilm.ts
+++ src/api/getFilm.ts
@@ -1,13 +1,14 @@
 const apiKey = import.meta.env.VITE_TMDB_API_KEY;
 
-
-export const getFilm = (id:number | string, media:string) => {
-    const link = `https://api.themoviedb.org/3/${media}/${id}?api_key=${apiKey}`
+export const getFilm = (id: number | string, media: string) => {
+    const link = `https://api.themoviedb.org/3/${media}/${id}?api_key=${apiKey}`;
 
     return fetch(link)
         .then((res) => {
             if (!res.ok) {
-                throw new Error(`Failed to fetch:${res.status} ${res.statusText}`);
+                throw new Error(
+                    `Failed to fetch:${res.status} ${res.statusText}`
+                );
             }
             return res.json();
         })
@@ -15,14 +16,19 @@ export const getFilm = (id:number | string, media:string) => {
             console.error(error);
             throw error;
         });
-}
+};
 export type filmDataT = {
     adult: boolean;
     backdrop_path: string | null;
-    belongs_to_collection: null | object
+    belongs_to_collection: null | {
+        id: number;
+        name: string;
+        poster_path: string;
+        backdrop_path: string;
+    };
     budget: number;
-    genres: {id: number, name: string}[];
-    homepage: string; 
+    genres: { id: number; name: string }[];
+    homepage: string;
     id: number;
     imdb_id: string | null;
     original_language: string;
@@ -35,7 +41,7 @@ export type filmDataT = {
         id: number;
         logo_path: string | null;
         origin_country: string;
-        }[];
+    }[];
     production_countries: {
         iso_3166_1: string;
         name: string;
@@ -57,46 +63,46 @@ export type filmDataT = {
     name: string;
 
     created_by: {
-        id: number,
-        credit_id: string,
-        name: string,
-        gender: number,
-        profile_path: string | null,
+        id: number;
+        credit_id: string;
+        name: string;
+        gender: number;
+        profile_path: string | null;
     }[];
-        episode_run_time: number[];
-        first_air_date: string;
-        in_productio: boolean;
-        language: string[];
-        last_air_date: string;
-        last_episode_to_air: {
-            air_date: string,
-            episode_number: number,
-            id: number,
-            name: string,
-            overview: string,
-            production_code: string,
-            season_number: number,
-            still_path: string | null,
-            vote_average: number,
-            vote_count: number,
-        };
-        networks: {
-            name: string,
-            id: number,
-            logo_path: string | null,
-            origin_country: string,
-        }[];
-        number_of_episodes: number;
-        number_of_seasons: number;
-        origin_country: string[];
-        original_name: string;
-        seasons: {
-            air_date: string,
-            episode_count: number,
-            id: number,
-            name: string,
-            overview: string,
-            poster_path: string,
-            season_number: number,
-        }[];
-}
+    episode_run_time: number[];
+    first_air_date: string;
+    in_productio: boolean;
+    language: string[];
+    last_air_date: string;
+    last_episode_to_air: {
+        air_date: string;
+        episode_number: number;
+        id: number;
+        name: string;
+        overview: string;
+        production_code: string;
+        season_number: number;
+        still_path: string | null;
+        vote_average: number;
+        vote_count: number;
+    };
+    networks: {
+        name: string;
+        id: number;
+        logo_path: string | null;
+        origin_country: string;
+    }[];
+    number_of_episodes: number;
+    number_of_seasons: number;
+    origin_country: string[];
+    original_name: string;
+    seasons: {
+        air_date: string;
+        episode_count: number;
+        id: number;
+        name: string;
+        overview: string;
+        poster_path: string;
+        season_number: number;
+    }[];
+};
