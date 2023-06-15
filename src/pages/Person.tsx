0c3775b16ec627eb70e8dccf9c28fccf69b39273
ComboTWTW <gfakemail18@gmail.com>
2023-06-15 23:57:25 +0500

    Person page layout

---
+++ src/pages/Person.tsx
@@ -0,0 +1,9 @@
+const Person = () => {
+    return (
+        <div className="w-full  flex justify-center text-center">
+            <div className="w-full max-w-[1300px] px-4 flex flex-col  items-center md:justify-center"></div>
+        </div>
+    );
+};
+
+export default Person;
