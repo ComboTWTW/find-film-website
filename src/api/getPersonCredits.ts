fe5f470e6248e60efd0b3683e7b08649757c2843
ComboTWTW <gfakemail18@gmail.com>
2023-06-17 23:54:14 +0500

    getPersonCredits type fix

--- src/api/getPersonCredits.ts
+++ src/api/getPersonCredits.ts
@@ -29,5 +29,28 @@ export type personCreditsT = {
         credit_id: string;
         order: number;
         media_type: string;
+        name: string;
     }[];
+    crew: {
+        adult: boolean;
+        backdrop_path: string;
+        genre_ids: number[];
+        id: number;
+        original_language: string;
+        original_title: string;
+        overview: string;
+        popularity: number;
+        poster_path: string;
+        release_date: string;
+        title: string;
+        video: boolean;
+        vote_average: number;
+        vote_count: number;
+        credit_id: string;
+        department: string;
+        job: string;
+        media_type: string;
+        name: string;
+    }[];
+    id: number;
 };
