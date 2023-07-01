06dc4ffe3cb61183db073ba2a605dcbed731176e
ComboTWTW <gfakemail18@gmail.com>
2023-07-01 17:43:40 +0500

    IncludeAdultSwitcher logic

--- src/components/MoviesShows/IncludeAdult.tsx
+++ src/components/MoviesShows/IncludeAdult.tsx
@@ -1,5 +1,6 @@
 import { filterSetsT } from "../../pages/MoviesShows";
 import Switch from "react-switch";
+import { useState, useEffect } from "react";
 
 interface Props {
     setFilterSets: Function;
@@ -7,6 +8,14 @@ interface Props {
 }
 
 const IncludeAdult = ({ setFilterSets, filterSets }: Props) => {
+    const [isInclude, setIsInclude] = useState<boolean>(true);
+
+    useEffect(() => {
+        setFilterSets({
+            ...filterSets,
+            includeAdult: `&include_adult=${isInclude}`,
+        });
+    }, [isInclude]);
     return (
         <div className="w-full flex flex-col gap-2">
             <h2 className="poppins text-white text-xl font-medium">
@@ -14,16 +23,29 @@ const IncludeAdult = ({ setFilterSets, filterSets }: Props) => {
             </h2>
 
             <div className="flex gap-3 items-center">
-                <h2 className="poppins text-white text-lg font-medium">No</h2>
+                <h2
+                    className={`poppins ${
+                        isInclude && "opacity-70"
+                    } text-white text-lg font-medium`}
+                >
+                    No
+                </h2>
                 <Switch
-                    checked={true}
-                    onChange={() => {}}
+                    checked={isInclude}
+                    onChange={() => setIsInclude((isInclude) => !isInclude)}
                     uncheckedIcon={false}
                     checkedIcon={false}
-                    height={30}
-                    width={60}
+                    height={35}
+                    width={65}
+                    handleDiameter={25}
                 />
-                <h2 className="poppins text-white text-lg font-medium">Yes</h2>
+                <h2
+                    className={`poppins ${
+                        !isInclude && "opacity-70"
+                    } text-white text-lg font-medium`}
+                >
+                    Yes
+                </h2>
             </div>
         </div>
     );
