948ca4a4eb56a8461e977acadb467fdc51de3de4
ComboTWTW <gfakemail18@gmail.com>
2023-07-09 23:50:26 +0500

    Result Selector Logic and API calling function fix

--- src/api/searchBig.ts
+++ src/api/searchBig.ts
@@ -2,7 +2,7 @@ const apiKey = import.meta.env.VITE_TMDB_API_KEY;
 
 export const searchBig = (input: string, media: string, page: number) => {
     const link = `https://api.themoviedb.org/3/search/${media}?api_key=${apiKey}&language=en-US&page=1&query=${input}&page=${page}`;
-    console.log(link);
+
     return fetch(link)
         .then((res) => res.json())
         .then((data) => data)
--- src/components/SearchBig/MediaType.tsx
+++ src/components/SearchBig/MediaType.tsx
@@ -1,25 +1,47 @@
 import { useState, useEffect } from "react";
 import { IoChevronDown, IoChevronUp } from "react-icons/io5";
 import { searchSetsT } from "../../pages/Search";
+import {
+    searchBigMovieT,
+    searchBigPersonT,
+    searchBigTvT,
+} from "../../api/searchBig";
 
 interface Props {
     searchSets: searchSetsT;
     setSearchSets: Function;
+    dataSearch: searchBigMovieT | searchBigPersonT | searchBigTvT;
 }
 
-const MediaType = ({ searchSets, setSearchSets }: Props) => {
+const MediaType = ({ searchSets, setSearchSets, dataSearch }: Props) => {
     const [toggleFilters, setToggleFilters] = useState<boolean>(true);
 
-    const sets: string[] = ["Movies", "TV Shows", "People"];
+    type setsT = { name: string; value: string };
+
+    const sets: setsT[] = [
+        { name: "Movies", value: "movie" },
+        { name: "TV Shows", value: "tv" },
+        { name: "People", value: "person" },
+    ];
+
+    const handleColor = (e: React.MouseEvent<HTMLButtonElement>): boolean => {
+        console.log(e.currentTarget.value === searchSets.media ? true : false);
+        return e.currentTarget.value === searchSets.media ? true : false;
+    };
+
+    const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+        setSearchSets({ ...searchSets, media: e.currentTarget.value });
+        console.log(e.currentTarget.value + " " + searchSets.media);
+    };
 
     return (
-        <div className="flex bg-darkLighter rounded-[5px] flex-col py-4 px-4">
+        <div className="flex bg-darkLighter rounded-[5px] flex-col py-4 ">
             {/* Header */}
             <div
                 onClick={() =>
                     setToggleFilters((toggleFilters) => !toggleFilters)
                 }
-                className="flex justify-between items-center"
+                className="flex justify-between items-center px-4"
             >
                 <h2 className="poppins text-white text-3xl md:text-3xl font-medium">
                     Results Type
@@ -34,16 +56,22 @@ const MediaType = ({ searchSets, setSearchSets }: Props) => {
                 </button>
             </div>
             {/* Filters*/}
-            <ul
-                className={`flex flex-col ${
-                    !toggleFilters && "hidden"
-                } mt-3 gap-4`}
-            >
-                {sets.map((set) => {
+            <ul className={`flex flex-col ${!toggleFilters && "hidden"} mt-5 `}>
+                {sets.map((set: setsT) => {
                     return (
-                        <li key={set} className="poppins text-white ">
-                            {set}
-                        </li>
+                        <button
+                            onClick={(e) => handleClick(e)}
+                            type="button"
+                            value={set.value}
+                            key={set.value}
+                            className={`poppins text-white text-xl w-full ${(
+                                event: React.MouseEvent<HTMLButtonElement>
+                            ) =>
+                                handleColor(event) &&
+                                "bg-white bg-opacity-30"} py-4 flex items-center justify-center`}
+                        >
+                            {set.name}
+                        </button>
                     );
                 })}
             </ul>
