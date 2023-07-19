a82545dc1e3cede8dfbf2639265f90be4683c308
Combo <gfakemail18@gmail.com>
2023-07-20 03:07:17 +0500

    Footer fix if content height less than screen height

--- src/App.tsx
+++ src/App.tsx
@@ -50,71 +50,81 @@ const App = () => {
     return (
         <QueryClientProvider client={queryClient}>
             <Router>
-                <div className="overflow-hidden w-full bg-bgMain min-h-screen">
+                <div className="flex flex-col min-h-screen overflow-hidden w-full bg-bgMain">
                     <Navbar />
-                    <Routes>
-                        {/* NavBar Pages */}
-                        <Route path={"/"} element={<Home />} />
-                        <Route
-                            path={"/movie/popular"}
-                            element={<MoviesShows />}
-                        />
-                        <Route
-                            path={"/movie/top-rated"}
-                            element={<MoviesShows />}
-                        />
-                        <Route path={"/tv/popular"} element={<MoviesShows />} />
-                        <Route
-                            path={"/tv/top-rated"}
-                            element={<MoviesShows />}
-                        />
-                        <Route path={"/people"} element={<People />} />
-                        <Route
-                            path={"/about"}
-                            element={<h1 className="text-white">about</h1>}
-                        />
+                    <div className="flex-grow overflow-hidden bg-bgMain">
+                        <Routes>
+                            {/* NavBar Pages */}
+                            <Route path={"/"} element={<Home />} />
+                            <Route
+                                path={"/movie/popular"}
+                                element={<MoviesShows />}
+                            />
+                            <Route
+                                path={"/movie/top-rated"}
+                                element={<MoviesShows />}
+                            />
+                            <Route
+                                path={"/tv/popular"}
+                                element={<MoviesShows />}
+                            />
+                            <Route
+                                path={"/tv/top-rated"}
+                                element={<MoviesShows />}
+                            />
+                            <Route path={"/people"} element={<People />} />
+                            <Route
+                                path={"/about"}
+                                element={<h1 className="text-white">about</h1>}
+                            />
 
-                        {/* Individual pages */}
-                        <Route path={"/movie/?"} element={<Movie />} />
-                        <Route path={"/tv/?"} element={<Movie />} />
-                        <Route path={"/person/?"} element={<Person />} />
-                        <Route path={"/search/?"} element={<Search />} />
+                            {/* Individual pages */}
+                            <Route path={"/movie/?"} element={<Movie />} />
+                            <Route path={"/tv/?"} element={<Movie />} />
+                            <Route path={"/person/?"} element={<Person />} />
+                            <Route path={"/search/?"} element={<Search />} />
 
-                        {/* Private (Show only if NOT logged in) */}
-                        <Route
-                            path={"/login"}
-                            element={
-                                isSigned === false ? (
-                                    <Login loginType={loginTypes.login} />
-                                ) : (
-                                    isSigned === true && <Navigate to="/" />
-                                )
-                            }
-                        />
-                        <Route
-                            path={"/signup"}
-                            element={
-                                isSigned === false ? (
-                                    <SignUp loginType={loginTypes.signup} />
-                                ) : (
-                                    isSigned === true && <Navigate to="/" />
-                                )
-                            }
-                        />
-                        {/* Private (Show only if logged in) */}
-                        <Route
-                            path={"/profile"}
-                            element={
-                                isSigned === true ? (
-                                    <Profile />
-                                ) : (
-                                    isSigned === false && <Navigate to="/" />
-                                )
-                            }
-                        />
+                            {/* Private (Show only if NOT logged in) */}
+                            <Route
+                                path={"/login"}
+                                element={
+                                    isSigned === false ? (
+                                        <Login loginType={loginTypes.login} />
+                                    ) : (
+                                        isSigned === true && <Navigate to="/" />
+                                    )
+                                }
+                            />
+                            <Route
+                                path={"/signup"}
+                                element={
+                                    isSigned === false ? (
+                                        <SignUp loginType={loginTypes.signup} />
+                                    ) : (
+                                        isSigned === true && <Navigate to="/" />
+                                    )
+                                }
+                            />
+                            {/* Private (Show only if logged in) */}
+                            <Route
+                                path={"/profile"}
+                                element={
+                                    isSigned === true ? (
+                                        <Profile />
+                                    ) : (
+                                        isSigned === false && (
+                                            <Navigate to="/" />
+                                        )
+                                    )
+                                }
+                            />
 
-                        <Route path={"*"} element={<h1>404 Not Found</h1>} />
-                    </Routes>
+                            <Route
+                                path={"*"}
+                                element={<h1>404 Not Found</h1>}
+                            />
+                        </Routes>
+                    </div>
                     <Footer />
                 </div>
             </Router>
